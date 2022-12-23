df1eb4033df0b2ad73468f9215666fc5d92d5e95
elbahjacharafeddine <charafensaj@gmail.com>
2022-12-23 23:23:24 +0100

    elbahj commit

---
+++ src/main/java/com/example/backendspring/beans/Restaurant.java
@@ -0,0 +1,19 @@
+package com.example.backendspring.beans;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
