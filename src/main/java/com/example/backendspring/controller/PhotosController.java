b7ed9395c7c6503a36ce5bb220b2444a341eee16
hanae <hanae.hanim01@gmail.com>
2022-12-27 13:10:59 +0100

    Photo

--- src/main/java/com/example/backendspring/controller/PhotosController.java
+++ src/main/java/com/example/backendspring/controller/PhotosController.java
@@ -2,10 +2,13 @@ package com.example.backendspring.controller;
 
 import com.example.backendspring.beans.Photo;
 import com.example.backendspring.beans.Serie;
+import com.example.backendspring.beans.Specialite;
 import com.example.backendspring.repository.PhotoRepository;
 import com.example.backendspring.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,4 +19,42 @@ public class PhotosController {
     public List<Photo> findAll(){
         return photoRepository.findAll();
     }
+
+    @GetMapping("/Photo/{id}")
+    public ResponseEntity<Photo> getPhotoByid(@PathVariable int id){
+
+        Photo photo = photoRepository.findById(id);
+
+        return ResponseEntity.ok(photo);
+
+    }
+    @GetMapping("/addPhoto")
+    public Photo AddSpeciality(@RequestBody Photo photo){
+        return photoRepository.save(photo);
+    }
+
+    //update bill by id
+    @PutMapping("/Photo/{id}")
+    public ResponseEntity<Photo> updatePhoto(@PathVariable int id,@RequestBody Photo photo){
+
+        Photo existE =photoRepository.findById(id);
+        existE.setUrl(photo.getUrl());
+        photoRepository.save(existE);
+        return ResponseEntity.ok(existE);
+
+    }
+
+    //Delete Photo By ID
+    @DeleteMapping("/Photo/{id}")
+    public ResponseEntity<HttpStatus> deleteBill(@PathVariable int id){
+
+        Photo photo = photoRepository.findById(id);
+
+        photoRepository.delete(photo);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+
+
 }
--- src/main/java/com/example/backendspring/repository/PhotoRepository.java
+++ src/main/java/com/example/backendspring/repository/PhotoRepository.java
@@ -1,8 +1,10 @@
 package com.example.backendspring.repository;
 
 import com.example.backendspring.beans.Photo;
+import com.example.backendspring.beans.Specialite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+    Photo findById(int id);
 }
 
