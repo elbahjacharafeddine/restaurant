73eaa289d6ae1080fa995b499a0b3af9490c1290
elbahjacharafeddine <charafensaj@gmail.com>
2022-12-27 11:23:49 +0100

    elbahja

--- src/main/java/com/example/backendspring/beans/Restaurant.java
+++ src/main/java/com/example/backendspring/beans/Restaurant.java
@@ -1,6 +1,9 @@
 package com.example.backendspring.beans;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Restaurant {
@@ -8,6 +11,21 @@ public class Restaurant {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
+    private String nom;
+    private String adresse;
+
+    private double latitude;
+    private double longitude;
+    private String rank;
+
+    @Temporal(TemporalType.TIME)
+    @JsonFormat(pattern="HH:mm")
+    private Date heureOpen;
+    @Temporal(TemporalType.TIME)
+    @JsonFormat(pattern="HH:mm")
+    private Date heureClose;
+
+
 
     public Long getId() {
         return id;
